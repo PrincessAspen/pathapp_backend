7f58ff3386ece7efce2766bafb2dd3d026cc5644
Aspen Berry <valiantagressor@gmail.com>
2024-11-14 09:38:48 -0500

    Got skills and feats functional

--- models/feats.py
+++ models/feats.py
@@ -9,3 +9,4 @@ class Feat(Base, table=True):
     description: str = Field(nullable=True, default="")
     numeric_modifier: Optional[float] = Field(nullable=True)
     level_requirement: int = Field(nullable=True, default=1)
+    category: str = Field(nullable=True, default="General")
